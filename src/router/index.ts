3b8b03a14029f12ea8d8a9058da504c6d8cd7632
Chaerok <rhffpa23@gmail.com>
2023-04-18 20:38:06 +0900

    last commit

--- src/router/index.ts
+++ src/router/index.ts
@@ -40,7 +40,7 @@ const routes: Array<RouteConfig> = [
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.NODE_ENV === 'production' ? '/board1/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/rwd/' : '/',
   routes
 })
 
